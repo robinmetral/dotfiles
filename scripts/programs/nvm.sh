08f7fc91ffcdcf9ee5b124b9fdca64f2ae679539
Robin Metral <robin.metral@sumup.com>
2020-06-04 22:01:55 +0200

    fix: load nvm manually after install

--- scripts/programs/nvm.sh
+++ scripts/programs/nvm.sh
@@ -8,13 +8,15 @@ function installnvm {
     echo "📦 installing nvm"
     # install according to https://github.com/nvm-sh/nvm
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+    # cannot use `source ~/.bashrc` here, loading nvm manually
+    # https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   fi
 }
 installnvm
-
-# alternative to `source ~/.bashrc` which doesn't work here
-# https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
-exec bash
 
 # install node
 nvm install node # latest version
