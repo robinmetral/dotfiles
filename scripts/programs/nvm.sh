af8fc4f61f9f5835db845a4d378ce1f790631135
Robin Metral <robin.metral@sumup.com>
2020-06-04 18:32:02 +0200

    feat: add nvm, node, yarn and global node modules

---
+++ scripts/programs/nvm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install nvm
+function installnvm {
+  if command -v code 2>/dev/null; then
+    echo "📦 nvm already installed"
+  else
+    echo "📦 installing nvm"
+    # install according to https://github.com/nvm-sh/nvm
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+  fi
+}
+installnvm
+
+source ~/.bashrc
+
+# install node
+nvm install node # latest version
+nvm install --lts # latest lts
---
+++ scripts/programs/yarn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install yarn
+function installyarn {
+  if command -v code 2>/dev/null; then
+    echo "📦 yarn already installed"
+  else
+    echo "📦 installing yarn"
+    # install according to https://classic.yarnpkg.com/en/docs/install/#alternatives-stable
+    wget -qO- https://yarnpkg.com/install.sh | bash
+  fi
+}
+
+# install global yarn packages
+function installmodule {
+  name="${1}"
+  yarn global add ${name}
+}
+# gatsby
+installmodule gatsby-cli
+gatsby telemetry --disable # disable telemetry
+# jest
+installmodule jest-cli
+# typescript
+installmodule typescript
